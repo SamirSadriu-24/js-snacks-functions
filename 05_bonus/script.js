7e18f10eb3f46f8aa293b93578a35211097d2153
AidenYukino <samirsadriuboolean@gmail.com>
2025-09-03 15:58:05 +0200

    05_bonus completato

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,20 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function greetingsByHour (name){
+    const ore = new Date().getHours();
+
+    if (ore <= 13 && ore >= 6){
+        return console.log("Buongiorno ", name);
+    } else if (ore >13 && ore <= 17){
+        return console.log("Buonpomeriggio ", name);
+    } else{
+        return console.log("Buonasera ", name);
+    }
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+greetingsByHour(name);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
