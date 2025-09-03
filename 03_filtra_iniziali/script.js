89003f75bf60cda29e7e57dd5e05b182063983f4
AidenYukino <samirsadriuboolean@gmail.com>
2025-09-03 15:29:10 +0200

    03_filtra_iniziali finito

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const userInitial = prompt("Dammi una lettera: ");
 
+console.log("Questa è la tua lettera scelta: " + userInitial)
+console.log("I nomi con quella lettera presenti nel database sono: ")
+
+function initialNames(names, userInitial,) {
+    filtredNames = [];
+
+    for (let i = 0; i < names.length; i++) {
+        var namesInitial = names[i][0].toUpperCase();
+        if (namesInitial === userInitial.toUpperCase()) {
+            filtredNames.push(names[i]);
+        }
+    }
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+initialNames(names, userInitial);
+console.log(filtredNames);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
