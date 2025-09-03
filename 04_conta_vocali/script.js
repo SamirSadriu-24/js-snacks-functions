9948b7335789d0cb2d12b2c8851a2c850c1306bf
AidenYukino <samirsadriuboolean@gmail.com>
2025-09-03 15:47:53 +0200

    04_conta_vocali finito

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,20 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowelsFinder (word){
+    let vowels = ["a", "i", "u", "e", "o"];
+    var vowelsInCommon = []
+    for (i = 0; i < word.length; i++){
+        if (vowels.includes(word[i])){
+           vowelsInCommon.push(word[i]);
+        }
+    }
+    return vowelsInCommon;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+vowelsFinder(word);
+console.log(word, ": ", (vowelsFinder(word)));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
